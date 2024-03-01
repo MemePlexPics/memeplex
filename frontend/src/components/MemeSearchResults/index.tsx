462af2c80f575c57d9c69f35e23fb926b4635c6a
Vishtar <viktor.kishchuk@gmail.com>
2024-03-01 14:06:31 +0400

    front: mobile memes gap bigger

--- frontend/src/components/MemeSearchResults/index.tsx
+++ frontend/src/components/MemeSearchResults/index.tsx
@@ -1,13 +1,14 @@
 import { IMeme } from "../../types"
 
-import './style.css'
 import { MemeContainer } from '..'
+import * as stylex from '@stylexjs/stylex'
+import { s } from "./style"
 
 export const MemeSearchResults = (props: { memes: IMeme[] }) => {
     return (
-        <div id="results">
+        <div {...stylex.props(s.results)}>
             {props.memes.map((meme) => (
-                <div key={meme.id} className="result-container">
+                <div key={meme.id} {...stylex.props(s.result)}>
                     <MemeContainer meme={meme} />
                 </div>
             ))}
