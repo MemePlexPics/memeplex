0303f003c66b2c74911371fd03e7cf0e1924b898
Vishtar <viktor.kishchuk@gmail.com>
2024-03-01 14:06:31 +0400

    front: mobile memes gap bigger

---
+++ frontend/src/components/MemeSearchResults/style.ts
@@ -0,0 +1,30 @@
+import * as stylex from '@stylexjs/stylex'
+import { size } from '../../styles/variables.stylex'
+
+const MOBILE_PORTRAIT = '@media only screen and (hover: none) and (pointer: coarse) and (orientation:portrait)'
+
+export const s = stylex.create({
+    results: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: {
+            default: '14px',
+            [MOBILE_PORTRAIT]: '3vw',
+        },
+        justifyContent: 'center',
+        paddingBottom: '80px',
+    },
+    result: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: {
+            default: size.memeEntity,
+            [MOBILE_PORTRAIT]: 'initial',
+        },
+        width: {
+            default: size.memeEntity,
+            [MOBILE_PORTRAIT]: '100%',
+        },
+    },
+})
