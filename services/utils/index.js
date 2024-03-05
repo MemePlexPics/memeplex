7cc269bb784b3db9d2b0c29c415cbb9bfbb892f4
Vishtar <viktor.kishchuk@gmail.com>
2024-03-05 16:50:45 +0400

    services: downloader, ocr ack timeout 10m (experimental)

--- services/utils/index.js
+++ services/utils/index.js
@@ -3,3 +3,4 @@ export { insertOcrKeysIntoDb } from './insertOcrKeysIntoDb.js';
 export { findNewProxies } from './findNewProxies.js';
 export { getProxies } from './getProxies.js';
 export { checkProxies } from './checkProxies.js';
+export { handleNackByTimeout } from './handleNackByTimeout.js';
