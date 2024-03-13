bad8c5750e2fa5a7092c1a50cccd177948a72891
Vishtar <viktor.kishchuk@gmail.com>
2024-03-13 12:23:20 +0400

    prettier, eslint configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
+// import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
-import styleX from "vite-plugin-stylex"
+import styleX from 'vite-plugin-stylex'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
   build: {
-      outDir: './dist',
+    outDir: './dist'
   },
   publicDir: './src/public',
-  plugins: [
-    react(),
-    styleX(),
-  ],
+  plugins: [react(), styleX()],
+  resolve: {
+    alias: {
+      '@': './src/' // resolve(__dirname, 'src'),
+    }
+  }
 })
