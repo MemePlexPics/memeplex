8b15e67f175d3111aa29ade6c01767199237bb58
Vishtar <viktor.kishchuk@gmail.com>
2024-03-15 00:41:07 +0400

    front: fix infinity scroll

--- frontend/src/hooks/useInfinityScroll.ts
+++ frontend/src/hooks/useInfinityScroll.ts
@@ -24,5 +24,5 @@ export const useInfinityScroll: TUseInfinityScroll = (callback, options) => {
     )
   }
 
-  useEventListener('scroll', onScroll)
+  useEventListener('scroll', onScroll, options?.element )
 }
