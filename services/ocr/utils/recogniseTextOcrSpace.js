74ee5506f2dae19346cd3f9b9e2614750078b0d1
Vishtar <viktor.kishchuk@gmail.com>
2024-03-18 12:42:57 +0400

    ocr: error.code into log

--- services/ocr/utils/recogniseTextOcrSpace.js
+++ services/ocr/utils/recogniseTextOcrSpace.js
@@ -2,9 +2,12 @@ import { ocrSpace, handleTimeout } from './index.js';
 import {
     chooseRandomOCRSpaceKey,
     getMysqlClient,
-    dateToYyyyMmDdHhMmSs
+    dateToYyyyMmDdHhMmSs,
 } from '../../../utils/index.js';
-import { saveKeyTimeout, setProxyAvailability } from '../../../utils/mysql-queries/index.js';
+import {
+    saveKeyTimeout,
+    setProxyAvailability,
+} from '../../../utils/mysql-queries/index.js';
 import { OCR_SPACE_403_DELAY } from '../../../constants/index.js';
 import { InfoMessage } from '../../../utils/index.js';
 
@@ -14,7 +17,7 @@ export const recogniseTextOcrSpace = async (fileName, language, logger) => {
         key: apiKey,
         timeout,
         proxy,
-        protocol
+        protocol,
     } = await chooseRandomOCRSpaceKey();
     if (timeout) handleTimeout(apiKey, timeout, logger);
     try {
@@ -25,43 +28,49 @@ export const recogniseTextOcrSpace = async (fileName, language, logger) => {
             host,
             port,
             protocol,
-            OCREngine: language == 'eng' ? '2' : '1'
+            OCREngine: language == 'eng' ? '2' : '1',
             // see here for engine descriptions: http://ocr.space/OCRAPI
         });
         console.log('💬', res);
-    } catch(error) {
+    } catch (error) {
         if (error?.response?.status === 403) {
             const mysql = await getMysqlClient();
-            const newTimeout = dateToYyyyMmDdHhMmSs(Date.now() + OCR_SPACE_403_DELAY);
+            const newTimeout = dateToYyyyMmDdHhMmSs(
+                Date.now() + OCR_SPACE_403_DELAY,
+            );
             await saveKeyTimeout(mysql, apiKey, newTimeout);
-            throw new InfoMessage(`❗️ 403 from ocr.space for key ${apiKey}, ${error.name}: ${error.message}`);
+            throw new InfoMessage(
+                `❗️ 403 from ocr.space for key ${apiKey}, ${error.name}: ${error.message}`,
+            );
         }
         if (
-            error.name === 'AxiosError'
-            || error.message === 'socket hang up'
-            || error.message === 'Socket closed'
-            || error.message === 'connect ETIMEDOUT'
-            || error.message === 'Proxy connection timed out'
-            || error.message.startsWith('Socks5 proxy rejected connection')
-            || error.message.startsWith('read ECONNRESET')
-            || error.message.startsWith('connect ECONNREFUSED')
-            || error.message.startsWith('connect ETIMEDOUT')
-            || error.message.startsWith('connect EHOSTUNREACH')
+            error.name === 'AxiosError' ||
+            error.message === 'socket hang up' ||
+            error.message === 'Socket closed' ||
+            error.message === 'Proxy connection timed out' ||
+            error.message.startsWith('Socks5 proxy rejected connection') ||
+            error.message.startsWith('read ECONNRESET') ||
+            error.message.startsWith('connect ECONNREFUSED') ||
+            error.code === 'ETIMEDOUT' ||
+            error.message.startsWith('connect EHOSTUNREACH')
         ) {
             const mysql = await getMysqlClient();
             await setProxyAvailability(mysql, proxy, protocol, false);
-            throw new InfoMessage(`Proxy error:, ${error.name} ${error.message}`);
+            throw new InfoMessage(
+                `Proxy error:, «${error.code}» ${error.name} ${error.message}`,
+            );
         }
         throw error;
     }
 
-    if (res.IsErroredOnProcessing)
-        throw new Error(res?.ErrorMessage?.join());
+    if (res.IsErroredOnProcessing) throw new Error(res?.ErrorMessage?.join());
 
     if (!Array.isArray(res?.ParsedResults)) {
         const mysql = await getMysqlClient();
         await setProxyAvailability(mysql, proxy, protocol, false);
-        throw new InfoMessage(`Invalid res.ParsedResults, probably dead proxy ${proxy} (${protocol}). ${JSON.stringify(res)}`);
+        throw new InfoMessage(
+            `Invalid res.ParsedResults, probably dead proxy ${proxy} (${protocol}). ${JSON.stringify(res)}`,
+        );
     }
 
     let text = [];
