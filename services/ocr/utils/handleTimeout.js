8982952b593c4ffff0f676b20ad627678f61362a
Vishtar <viktor.kishchuk@gmail.com>
2024-03-19 20:14:24 +0400

    ocr: refactoring

--- services/ocr/utils/handleTimeout.js
+++ services/ocr/utils/handleTimeout.js
@@ -4,13 +4,13 @@ import { saveKeyTimeout } from '../../../utils/mysql-queries/index.js';
 export const handleTimeout = async (apiKey, timeout, logger) => {
     const utcNow = getDateUtc();
     const delayMs = Math.max(0, new Date(timeout) - utcNow);
-    const mysql = await getMysqlClient({ connectTimeout: delayMs + 5_000 });
     if (delayMs > 0) {
         logger.info(
             `💬 Key timeout: ${timeout}. Wait ${delayMs / 1000} seconds`,
         );
         await delay(delayMs);
     }
+    const mysql = await getMysqlClient();
     await saveKeyTimeout(mysql, apiKey, null);
     mysql.close();
 };
