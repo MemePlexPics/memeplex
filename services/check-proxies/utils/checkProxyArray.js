9dc7d27bf8b9351dc34a15703965747f54cbf03b
Vishtar <viktor.kishchuk@gmail.com>
2024-03-19 12:53:43 +0400

    check-proxies: refactoring

---
+++ services/check-proxies/utils/checkProxyArray.js
@@ -0,0 +1,17 @@
+import { checkProxy } from '../utils/index.js';
+import { updateProxy } from '../../../utils/mysql-queries/index.js';
+
+export const checkProxyArray = async (mysql, proxies, ipWithoutProxy, logger) => {
+    for (const proxy of proxies) {
+        const result = await checkProxy(proxy, ipWithoutProxy, logger);
+        await updateProxy(
+            mysql,
+            `${proxy.ip}:${proxy.port}`,
+            proxy.protocol,
+            result.availability,
+            result.anonymity,
+            result.speed,
+            result.lastCheckDatetime,
+        );
+    }
+};
