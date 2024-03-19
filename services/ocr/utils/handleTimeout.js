d04e0e6b976b25ad5e4e5ea3cb37cc221b61b452
Vishtar <viktor.kishchuk@gmail.com>
2024-03-19 16:30:18 +0400

    ocr: black list

--- services/ocr/utils/handleTimeout.js
+++ services/ocr/utils/handleTimeout.js
@@ -2,15 +2,15 @@ import { delay, getMysqlClient, getDateUtc } from '../../../utils/index.js';
 import { saveKeyTimeout } from '../../../utils/mysql-queries/index.js';
 
 export const handleTimeout = async (apiKey, timeout, logger) => {
-    const mysql = await getMysqlClient();
     const utcNow = getDateUtc();
     const delayMs = Math.max(0, new Date(timeout) - utcNow);
-    if (delayMs !== 0) {
+    const mysql = await getMysqlClient({ connectTimeout: delayMs + 5_000 });
+    if (delayMs > 0) {
         logger.info(
             `💬 Key timeout: ${timeout}. Wait ${delayMs / 1000} seconds`,
         );
-        logger.error('There are no keys without timeout');
         await delay(delayMs);
     }
     await saveKeyTimeout(mysql, apiKey, null);
+    mysql.close();
 };
