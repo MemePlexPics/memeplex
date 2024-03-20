c984eb55805a3f100cf8d30d66d9f56b579de52c
Vishtar <viktor.kishchuk@gmail.com>
2024-03-20 17:17:33 +0400

    tg-publisher: schema update

---
+++ db/schema/botPublisherKeywords.ts
@@ -0,0 +1,7 @@
+import { mysqlTable, int, varchar, serial } from "drizzle-orm/mysql-core"
+
+export const botPublisherKeywords = mysqlTable("bot_publisher_keywords", {
+	id: serial("id").primaryKey(),
+	keyword: varchar("keyword", { length: 255 }).notNull(),
+	lastCheckTimestamp: int("timestamp").notNull(),
+})
