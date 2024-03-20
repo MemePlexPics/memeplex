615ed8ca5413257774647b8d0eabfff98ab97509
Vishtar <viktor.kishchuk@gmail.com>
2024-03-20 14:13:22 +0400

    tg-bot: refactoring

---
+++ services/servers/tg-bots/pics/handlers/onBotCommandSuggestChannel.js
@@ -0,0 +1,21 @@
+import { getMysqlClient, getTgChannelName, logError } from '../../../../../utils/index.js';
+import { insertChannelSuggestion } from '../../../../../utils/mysql-queries/index.js';
+import { logUserAction } from '../utils/index.js';
+
+export const onBotCommandSuggestChannel = async (ctx, logger) => {
+    const { payload } = ctx;
+    const channelName = getTgChannelName(payload.trim());
+    if (!channelName)
+        return ctx.reply(`You must specify the channel name:
+/suggest_channel name`);
+    try {
+        const mysql = await getMysqlClient();
+        const response = await insertChannelSuggestion(mysql, channelName);
+        if (response)
+            logUserAction(ctx.from, { info: `suggested @${channelName}` });
+        return ctx.reply('Thank you for the suggestion!');
+    } catch (e) {
+        logError(logger, e);
+        await ctx.reply('An error occurred, please try again later');
+    }
+};
