b5d0b41445fba1d8f6ad60d2b284e96757f51b8a
Vishtar <viktor.kishchuk@gmail.com>
2024-03-20 14:13:37 +0400

    prettier

--- scripts/db-migrate.ts
+++ scripts/db-migrate.ts
@@ -1,16 +1,16 @@
-import { migrate } from 'drizzle-orm/mysql2/migrator';
-import { drizzle } from 'drizzle-orm/mysql2';
-import { getMysqlClient } from '../utils';
-import * as schema from '../db/schema';
+import { migrate } from 'drizzle-orm/mysql2/migrator'
+import { drizzle } from 'drizzle-orm/mysql2'
+import { getMysqlClient } from '../utils'
+import * as schema from '../db/schema'
 
-const client = await getMysqlClient();
+const client = await getMysqlClient()
 
 await migrate(
-    drizzle(client, {
-        schema,
-        mode: "default",
-    }),
-    { migrationsFolder: './db/migrations' },
-);
+  drizzle(client, {
+    schema,
+    mode: 'default'
+  }),
+  { migrationsFolder: './db/migrations' }
+)
 
-await client.end();
+await client.end()
