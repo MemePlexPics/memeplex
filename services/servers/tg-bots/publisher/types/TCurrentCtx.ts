377ef8878fa0931fc4bb741babd508af8d589670
Vishtar <viktor.kishchuk@gmail.com>
2024-03-21 18:16:03 +0400

    tg-publisher: wip

--- services/servers/tg-bots/publisher/types/TCurrentCtx.ts
+++ services/servers/tg-bots/publisher/types/TCurrentCtx.ts
@@ -3,5 +3,7 @@ import { DefaultCtx, GenericMenu } from 'telegraf-menu'
 export type TCurrentCtx = DefaultCtx & {
   session: {
     keyboardMenu: GenericMenu
+    channel?: string
+    state: string
   }
 }
