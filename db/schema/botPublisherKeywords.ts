2905d9d9a7c90c3bc872bc2c8dcf07bc8027b743
Vishtar <viktor.kishchuk@gmail.com>
2024-03-27 18:46:16 +0400

    tg-publisher: migrations file

--- db/schema/botPublisherKeywords.ts
+++ db/schema/botPublisherKeywords.ts
@@ -1,7 +1,7 @@
 import { mysqlTable, int, varchar, serial } from "drizzle-orm/mysql-core"
 
 export const botPublisherKeywords = mysqlTable("bot_publisher_keywords", {
-	id: serial("id").primaryKey(),
+	id: int("id").autoincrement().primaryKey(),
 	keyword: varchar("keyword", { length: 255 }).unique().notNull(),
 	// lastCheckTimestamp: int("timestamp"),
 })
--- services/servers/tg-bots/publisher/states/mainState.ts
+++ services/servers/tg-bots/publisher/states/mainState.ts
@@ -3,16 +3,29 @@ import { EState } from "../constants"
 import { TState } from "../types"
 import { enterToState } from "../utils"
 import { addChannelState, channelSelectState } from "."
+import { drizzle } from "drizzle-orm/mysql2"
+import { getMysqlClient } from '../../../../../utils'
+import { botPublisherChannels } from "../../../../../db/schema"
+import { count, eq } from "drizzle-orm"
 
 export const mainState: TState<EState> = {
     stateName: EState.MAIN,
-    inlineMenu: () => ({
-        text: 'Добро пожаловать в MemePlex Publisher!',
-        buttons: [
-            Key.callback('Добавить канал', EState.ADD_CHANNEL),
-            Key.callback('Настройки каналов', EState.CHANNEL_SELECT),
-        ],
-    }),
+    inlineMenu: async (ctx) => {
+        const db = drizzle(await getMysqlClient())
+        const userChannels = await db
+            .select({ values: count() })
+            .from(botPublisherChannels)
+            .where(eq(botPublisherChannels.userId, ctx.from.id))
+        return {
+            text: 'Добро пожаловать в MemePlex Publisher!',
+            buttons: [
+                Key.callback('Добавить канал', EState.ADD_CHANNEL),
+                userChannels[0].values !== 0
+                    ? Key.callback('Настройки каналов', EState.CHANNEL_SELECT)
+                    : undefined,
+            ],
+        }
+    },
     onCallback: async (ctx, state) => {
         if (state === EState.ADD_CHANNEL) {
             await enterToState(ctx, addChannelState)
