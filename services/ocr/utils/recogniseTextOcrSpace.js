f02de6f657d0cf10e1a5bffab77800d26414dfb0
Vishtar <viktor.kishchuk@gmail.com>
2024-03-27 14:42:06 +0400

    ocr: 'connect ECONNREFUSED' error handle

--- services/ocr/utils/recogniseTextOcrSpace.js
+++ services/ocr/utils/recogniseTextOcrSpace.js
@@ -54,7 +54,8 @@ export const recogniseTextOcrSpace = async (fileName, language, logger) => {
             error.message === 'socket hang up' ||
             error.message === 'Socket closed' ||
             error.message === 'Proxy connection timed out' ||
-            error.message.startsWith('Socks5 proxy rejected connection')
+            error.message.startsWith('Socks5 proxy rejected connection') ||
+            error.message.startsWith('connect ECONNREFUSED') // somehow this is not the same error as the error.code === 'ECONNREFUSED'
         ) {
             const mysql = await getMysqlClient();
             await updateProxyAvailability(mysql, proxy, protocol, false);
