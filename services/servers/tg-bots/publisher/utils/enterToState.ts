5ae0290da5f54658cb169473c3b995d64b4e234d
Vishtar <viktor.kishchuk@gmail.com>
2024-03-27 17:17:07 +0400

    tg-publisher: wip

--- services/servers/tg-bots/publisher/utils/enterToState.ts
+++ services/servers/tg-bots/publisher/utils/enterToState.ts
@@ -5,7 +5,7 @@ import { EState } from "../constants"
 export const enterToState = async <GStateName extends EState>(ctx: TTelegrafContext, state: TState<GStateName>) => {
     ctx.session.state = state.stateName
     if (state.inlineMenu) {
-        const inlineMenu = state.inlineMenu(ctx)
+        const inlineMenu = await state.inlineMenu(ctx)
         await ctx.reply(inlineMenu.text, Keyboard.make(inlineMenu.buttons).inline())
         const message = state.message?.(ctx)
         if (message) ctx.reply(message)
