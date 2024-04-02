a0823b99a6885a4ff769e55e3eeb681e334c86df
Vishtar <viktor.kishchuk@gmail.com>
2024-04-02 16:05:35 +0400

    tg-pics: latest selected if no query

--- services/servers/tg-bots/pics/handlers/index.js
+++
@@ -1,4 +0,0 @@
-export { onBotRecieveText } from './onBotRecieveText';
-export { onBotCommandGetLatest } from './onBotCommandGetLatest';
-export { onBotCommandSuggestChannel } from './onBotCommandSuggestChannel';
-export { onInlineQuery } from './onInlineQuery';
---
+++ services/servers/utils/getMemesById.ts
@@ -0,0 +1,20 @@
+import { ELASTIC_INDEX } from '../../../constants'
+import { getMemeResponseEntity } from '.'
+import { Client } from '@elastic/elasticsearch'
+import { TElasticMemeEntity } from '../../types'
+import { GetGetResult } from '@elastic/elasticsearch/lib/api/types'
+
+export const getMemesById = async (client: Client, ids: string[], abortController?: AbortController) => {
+  const elasticRes = await client.mget<TElasticMemeEntity>({
+    index: ELASTIC_INDEX,
+    ids
+  }, abortController
+    ? { signal: abortController.signal }
+    : {},
+  )
+
+  return elasticRes.docs.map(doc => {
+    if (!Object.hasOwn(doc, 'found')) return
+    return getMemeResponseEntity(doc._id, (doc as GetGetResult<TElasticMemeEntity>)._source)
+  })
+}
