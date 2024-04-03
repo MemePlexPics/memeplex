f695b2294f23ce3a881d982a22fa3f3abe001223
Vishtar <viktor.kishchuk@gmail.com>
2024-04-03 14:50:18 +0400

    elastic-certs: fix

--- scripts/elastice-certs.sh
+++ scripts/elastice-certs.sh
@@ -6,4 +6,6 @@ docker run --rm -v $(pwd)/certs:/certs docker.elastic.co/elasticsearch/elasticse
 docker run --rm -v $(pwd)/certs:/certs docker.elastic.co/elasticsearch/elasticsearch:8.11.3 \
 /bin/bash -c 'elasticsearch-certutil cert --ca /certs/elastic-stack-ca.p12 --ca-pass "" --name memesearch-elastic --dns memesearch-elastic --ip 0.0.0.0 --out /certs/elastic-certificates.p12 --pass "" --silent'
 
-openssl pkcs12 -in certs/elastic-certificates.p12 -clcerts -nokeys -out certs/elastic-certificates.crt
+openssl pkcs12 -in certs/elastic-stack-ca.p12 -clcerts -nokeys -out certs/elastic-stack-ca.pem -passin pass:
+openssl pkcs12 -in certs/elastic-certificates.p12 -nocerts -nodes -out certs/elastic-certificates.key -passin pass:
+openssl pkcs12 -in certs/elastic-certificates.p12 -clcerts -nokeys -out certs/elastic-certificates.crt -passin pass:
