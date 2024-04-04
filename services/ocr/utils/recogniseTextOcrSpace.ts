5bf390ea8e640da20b6587ca692b8e61957dc7ce
Vishtar <viktor.kishchuk@gmail.com>
2024-04-04 13:43:05 +0400

    ocr: pro key endpoint, refactoring

--- services/ocr/utils/recogniseTextOcrSpace.ts
+++ services/ocr/utils/recogniseTextOcrSpace.ts
@@ -1,25 +1,24 @@
-import { chooseRandomOCRSpaceKey, ocrSpace } from '.'
+import { chooseRandomOCRSpaceKey, handle403, handleDeadProxy, handleProxyError, ocrSpace } from '.'
 import {
   getMysqlClient,
-  dateToYyyyMmDdHhMmSs,
 } from '../../../utils'
 import {
-  saveKeyTimeout,
   updateProxyAvailability,
 } from '../../../utils/mysql-queries'
-import { OCR_SPACE_403_DELAY } from '../../../constants'
-import { InfoMessage } from '../../../utils'
+import { OCR_SPACE_PRO_API_USA } from '../../../constants'
+import { Logger } from 'winston'
 
-export const recogniseTextOcrSpace = async (fileName, language) => {
-  let res
+export const recogniseTextOcrSpace = async (fileName: string, language: string, logger: Logger) => {
   const {
     key: apiKey,
     proxy,
     protocol,
   } = await chooseRandomOCRSpaceKey()
+  logger.verbose(`💬 OCR: ${apiKey} ${proxy ? `${proxy} (${protocol})` : ''}`)
   try {
     const [host, port] = proxy ? proxy.split(':') : [null, null]
-    res = await ocrSpace(fileName, {
+    const res = await ocrSpace(fileName, {
+      ocrUrl: proxy ? undefined : OCR_SPACE_PRO_API_USA,
       apiKey,
       language,
       proxy: proxy
@@ -33,64 +32,25 @@ export const recogniseTextOcrSpace = async (fileName, language) => {
       // see here for engine descriptions: http://ocr.space/OCRAPI
     })
     console.log('💬', res)
+
+    if (res.IsErroredOnProcessing) throw new Error(res?.ErrorMessage?.join())
+    await handleDeadProxy(res, proxy, protocol)
+
+    if (proxy) {
+      const mysql = await getMysqlClient()
+      await updateProxyAvailability(mysql, proxy, protocol, true)
+      mysql.end()
+    }
+
+    const text = []
+    for (const result of res.ParsedResults) {
+      text.push(result.ParsedText.replace(/\r\n/g, ' '))
+    }
+
+    return text.join(' ')
   } catch (error) {
-    if (error?.response?.status === 403) {
-      const newTimeout = dateToYyyyMmDdHhMmSs(
-        Date.now() + OCR_SPACE_403_DELAY,
-      )
-      const mysql = await getMysqlClient()
-      await saveKeyTimeout(mysql, apiKey, newTimeout)
-      mysql.end()
-      throw new InfoMessage(
-        `❗️ 403 from ocr.space for key ${apiKey}, ${error.name}: ${error.message}`,
-      )
-    }
-    if (
-      proxy && (
-        error.name === 'AxiosError' ||
-        error.code === 'ECONNREFUSED' ||
-        error.code === 'ERR_BAD_REQUEST' ||
-        error.code === 'ECONNRESET' ||
-        error.code === 'ETIMEDOUT' ||
-        error.code === 'EHOSTUNREACH' ||
-        error.message === 'socket hang up' ||
-        error.message === 'Socket closed' ||
-        error.message === 'Proxy connection timed out' ||
-        error.message.startsWith('Socks5 proxy rejected connection') ||
-        error.message.startsWith('connect ECONNREFUSED') || // somehow this is not the same error as the error.code === 'ECONNREFUSED'
-        error.message.startsWith('read ECONNRESET') // the same as above
-      )
-    ) {
-      const mysql = await getMysqlClient()
-      await updateProxyAvailability(mysql, proxy, protocol, false)
-      mysql.end()
-      throw new InfoMessage(
-        `Proxy error:, «${error.code}» ${error.name} ${error.message}`,
-      )
-    }
+    await handle403(error, apiKey)
+    await handleProxyError(error, proxy, protocol)
     throw error
   }
-
-  if (res.IsErroredOnProcessing) throw new Error(res?.ErrorMessage?.join())
-
-  if (proxy && !Array.isArray(res?.ParsedResults)) {
-    const mysql = await getMysqlClient()
-    await updateProxyAvailability(mysql, proxy, protocol, false)
-    mysql.end()
-    throw new InfoMessage(
-      `Invalid res.ParsedResults, probably dead proxy ${proxy} (${protocol}). ${JSON.stringify(res)}`,
-    )
-  }
-
-  if (proxy) {
-    const mysql = await getMysqlClient()
-    await updateProxyAvailability(mysql, proxy, protocol, true)
-    mysql.end()
-  }
-
-  const text = []
-  for (const result of res.ParsedResults) {
-    text.push(result.ParsedText.replace(/\r\n/g, ' '))
-  }
-  return text.join(' ')
 }
