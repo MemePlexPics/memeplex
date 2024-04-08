a4f1bd59626b6e110307427a1a6e7cb908e10f72
Vishtar <viktor.kishchuk@gmail.com>
2024-04-08 16:49:02 +0400

    tg-publisher: wip

--- services/servers/tg-bots/publisher/utils/enterToState.ts
+++ services/servers/tg-bots/publisher/utils/enterToState.ts
@@ -8,16 +8,6 @@ export const enterToState = async <GStateName extends EState>(
   state: TState<GStateName>,
 ) => {
   ctx.session.state = state.stateName
-  if (state.inlineMenu) {
-    const inlineMenu = await state.inlineMenu(ctx)
-    const menu = await ctx.reply(inlineMenu.text, Keyboard.make(inlineMenu.buttons).inline())
-    if (ctx.session.lastMenuId) {
-      await ctx.deleteMessage(ctx.session.lastMenuId)
-    }
-    ctx.session.lastMenuId = menu.message_id
-    const message = state.message?.(ctx)
-    if (message) await ctx.reply(message)
-  }
   if (state.menu) {
     const onTextOptions: Record<string, (ctx?: TTelegrafContext) => Promisable<unknown>> = {}
     const { text: menuText, buttons: buttonsRaw } = await state.menu(ctx)
@@ -37,5 +27,18 @@ export const enterToState = async <GStateName extends EState>(
       await state.onText(ctx, text)
     }
     await ctx.reply(menuText, Keyboard.make(buttons).reply())
+  }
+  if (state.inlineMenu) {
+    const inlineMenu = await state.inlineMenu(ctx)
+    // @ts-expect-error remove_keyboard bullshit, questinable type unition
+    const menu = await ctx.reply(inlineMenu.text, new Keyboard([], {
+      one_time_keyboard: true,
+    }).make(inlineMenu.buttons).inline())
+    // if (ctx.session.lastMenuId) {
+    //   await ctx.deleteMessage(ctx.session.lastMenuId)
+    // }
+    // ctx.session.lastMenuId = menu.message_id
+    const message = state.message?.(ctx)
+    if (message) await ctx.reply(message)
   }
 }
