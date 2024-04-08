8019356b612e44c22274684825053989f9aa5654
Vishtar <viktor.kishchuk@gmail.com>
2024-04-08 15:39:18 +0400

    tg-publisher: fix

--- services/servers/tg-bots/publisher/types/TState.ts
+++ services/servers/tg-bots/publisher/types/TState.ts
@@ -1,9 +1,10 @@
-import { TInlineMenu, TMenuButton, TTelegrafContext } from '.'
+import { TInlineMenu, TMenu, TTelegrafContext } from '.'
+import { Promisable } from '../../../../../types'
 
 export type TState<GStateName> = {
   stateName: GStateName
-  menu?: (ctx?: TTelegrafContext) => Promise<TMenuButton[][]> | TMenuButton[][]
-  inlineMenu?: (ctx?: TTelegrafContext) => Promise<TInlineMenu> | TInlineMenu
+  menu?: (ctx?: TTelegrafContext) => Promisable<TMenu>
+  inlineMenu?: (ctx?: TTelegrafContext) => Promisable<TInlineMenu>
   message?: (ctx?: TTelegrafContext) => string
   onCallback?: <GCallback = GStateName>(ctx: TTelegrafContext, callback: GCallback) => unknown
   onText?: (ctx?: TTelegrafContext, text?: string) => unknown
