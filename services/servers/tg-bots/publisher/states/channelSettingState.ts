97b3f23f7f4438b55a90439edbdc2fa5d33ccbd6
Vishtar <viktor.kishchuk@gmail.com>
2024-04-08 16:36:33 +0400

    tg-publisher: more menus

--- services/servers/tg-bots/publisher/states/channelSettingState.ts
+++ services/servers/tg-bots/publisher/states/channelSettingState.ts
@@ -1,62 +1,44 @@
-import { Key } from 'telegram-keyboard'
 import { EState } from '../constants'
-import { TState, TTelegrafContext } from '../types'
-import { enterToState } from '../utils'
+import { TMenuButton, TState } from '../types'
+import { enterToState, onClickDeleteChannel } from '../utils'
 import { addKeywordsState, keywordSettingsState, mainState } from '.'
-import { InfoMessage, getDbConnection } from '../../../../../utils'
+import { getDbConnection } from '../../../../../utils'
 import {
   countPublisherSubscriptionsByChannelId,
-  deletePublisherChannelById,
-  deletePublisherSubscriptionsByChannelId,
 } from '../../../../../utils/mysql-queries'
-
-const DELETE_CHANNEL = 'delete_channel'
 
 export const channelSettingState: TState<EState> = {
   stateName: EState.CHANNEL_SETTINGS,
-  inlineMenu: async ctx => {
+  menu: async (ctx) => {
     const db = await getDbConnection()
     const keywordsCount = await countPublisherSubscriptionsByChannelId(db, ctx.session.channel.id)
     await db.close()
     const hasKeywords = keywordsCount !== 0
-    const buttons = [
-      [Key.callback('➕ Добавить ключевые слова', EState.ADD_KEYWORDS)],
-      [Key.callback('🗑 Отвязать канал', DELETE_CHANNEL)],
-      [Key.callback('🏠 В главное меню', EState.MAIN)],
+    const buttons: TMenuButton[][] = [
+      [
+        ['➕ Добавить ключевые слова', (ctx) => enterToState(ctx, addKeywordsState)],
+      ],
+      [
+        ['🗑 Отвязать канал', async (ctx) => {
+          await onClickDeleteChannel(ctx)
+          ctx.session.channel = undefined
+          await enterToState(ctx, mainState)
+        }]
+      ],
+      [
+        ['🏠 В главное меню', async (ctx) => {
+          ctx.session.channel = undefined
+          await enterToState(ctx, mainState)
+        }]
+      ],
     ]
     if (hasKeywords)
       buttons.splice(1, 0, [
-        Key.callback('✏️ Редактировать ключевые слова', EState.KEYWORD_SETTINGS),
+        ['✏️ Редактировать ключевые слова', (ctx) => enterToState(ctx, keywordSettingsState)],
       ])
     return {
       text: `Настройка подписки ${ctx.session.channel.name}`,
       buttons,
     }
-  },
-  onCallback: async <EState>(ctx: TTelegrafContext, callback: EState | string) => {
-    if (callback === EState.ADD_KEYWORDS) {
-      await enterToState(ctx, addKeywordsState)
-      return
-    }
-    if (callback === EState.KEYWORD_SETTINGS) {
-      await enterToState(ctx, keywordSettingsState)
-      return
-    }
-    if (callback === DELETE_CHANNEL) {
-      const db = await getDbConnection()
-      await deletePublisherSubscriptionsByChannelId(db, ctx.session.channel.id)
-      await deletePublisherChannelById(db, ctx.session.channel.id)
-      await db.close()
-      await ctx.reply(`Канал успешно удален`)
-      ctx.session.channel = undefined
-      await enterToState(ctx, mainState)
-      return
-    }
-    if (callback === EState.MAIN) {
-      ctx.session.channel = undefined
-      await enterToState(ctx, mainState)
-      return
-    }
-    throw new InfoMessage(`Unknown menu state: ${callback}`)
   },
 }
--- services/servers/tg-bots/publisher/states/mainState.ts
+++ services/servers/tg-bots/publisher/states/mainState.ts
@@ -1,14 +1,11 @@
-import { Key } from 'telegram-keyboard'
 import { EState } from '../constants'
 import { TMenuButton, TState } from '../types'
 import { enterToState, onClickAddMyself } from '../utils'
 import { addChannelState, addKeywordsState, channelSelectState } from '.'
-import { InfoMessage, getDbConnection } from '../../../../../utils'
+import { getDbConnection } from '../../../../../utils'
 import {
   countPublisherChannelsByUserId,
 } from '../../../../../utils/mysql-queries'
-
-const ADD_MYSELF = 'add_myself'
 
 export const mainState: TState<EState> = {
   stateName: EState.MAIN,
@@ -30,35 +27,4 @@ export const mainState: TState<EState> = {
       buttons,
     }
   },
-  // inlineMenu: async ctx => {
-  //   const db = await getDbConnection()
-  //   const userChannelsCount = await countPublisherChannelsByUserId(db, ctx.from.id)
-  //   await db.close()
-  //   const buttons = [[Key.callback('Добавить канал', EState.ADD_CHANNEL)]]
-  //   if (userChannelsCount !== 0) {
-  //     buttons.push([Key.callback('Настройки подписок', EState.CHANNEL_SELECT)])
-  //   } else {
-  //     buttons.push([Key.callback('Добавить ключевые слова', ADD_MYSELF)])
-  //   }
-  //   return {
-  //     text: 'Меню подписок',
-  //     buttons,
-  //   }
-  // },
-  // onCallback: async (ctx, state) => {
-  //   if (state === ADD_MYSELF) {
-  //     await onClickAddMyself(ctx)
-  //     await enterToState(ctx, addKeywordsState)
-  //     return
-  //   }
-  //   if (state === EState.ADD_CHANNEL) {
-  //     await enterToState(ctx, addChannelState)
-  //     return
-  //   }
-  //   if (state === EState.CHANNEL_SELECT) {
-  //     await enterToState(ctx, channelSelectState)
-  //     return
-  //   }
-  //   throw new InfoMessage(`Unknown menu state: ${state}`)
-  // },
 }
