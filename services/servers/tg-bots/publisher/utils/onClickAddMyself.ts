87466e7bed4a9bb99471a91bbf98ce56f01f450b
Vishtar <viktor.kishchuk@gmail.com>
2024-04-08 15:13:58 +0400

    tg-publisher: menu buttons

---
+++ services/servers/tg-bots/publisher/utils/onClickAddMyself.ts
@@ -0,0 +1,23 @@
+import { getDbConnection } from "../../../../../utils"
+import { insertPublisherChannel } from "../../../../../utils/mysql-queries"
+import { getTelegramUser } from "../../utils"
+import { TTelegrafContext } from "../types"
+
+export const onClickAddMyself = async (ctx: TTelegrafContext) => {
+  const username = getTelegramUser(ctx.from, '')
+  ctx.session.channel = {
+    name: username.user,
+    id: ctx.from.id,
+    type: 'private',
+  }
+  const db = await getDbConnection()
+  const timestamp = Date.now() / 1000
+  await insertPublisherChannel(db, {
+    id: ctx.from.id,
+    userId: ctx.from.id,
+    username: username.user,
+    subscribers: 0,
+    timestamp,
+  })
+  await db.close()
+}
