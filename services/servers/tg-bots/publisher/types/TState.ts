e8dfaa373c2c276a4ff0f6553f81d678f3e59d66
Vishtar <viktor.kishchuk@gmail.com>
2024-04-08 15:13:58 +0400

    tg-publisher: menu buttons

--- services/servers/tg-bots/publisher/types/TState.ts
+++ services/servers/tg-bots/publisher/types/TState.ts
@@ -1,7 +1,8 @@
-import { TInlineMenu, TTelegrafContext } from '.'
+import { TInlineMenu, TMenuButton, TTelegrafContext } from '.'
 
 export type TState<GStateName> = {
   stateName: GStateName
+  menu?: (ctx?: TTelegrafContext) => Promise<TMenuButton[][]> | TMenuButton[][]
   inlineMenu?: (ctx?: TTelegrafContext) => Promise<TInlineMenu> | TInlineMenu
   message?: (ctx?: TTelegrafContext) => string
   onCallback?: <GCallback = GStateName>(ctx: TTelegrafContext, callback: GCallback) => unknown
