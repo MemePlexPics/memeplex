a2fabfc52f9c67df9a3bdb3e671d71138ce931c9
Vishtar <viktor.kishchuk@gmail.com>
2024-04-08 15:13:58 +0400

    tg-publisher: menu buttons

--- services/servers/tg-bots/publisher/states/mainState.ts
+++ services/servers/tg-bots/publisher/states/mainState.ts
@@ -1,62 +1,61 @@
 import { Key } from 'telegram-keyboard'
 import { EState } from '../constants'
-import { TState } from '../types'
-import { enterToState } from '../utils'
+import { TMenuButton, TState } from '../types'
+import { enterToState, onClickAddMyself } from '../utils'
 import { addChannelState, addKeywordsState, channelSelectState } from '.'
 import { InfoMessage, getDbConnection } from '../../../../../utils'
 import {
   countPublisherChannelsByUserId,
-  insertPublisherChannel,
 } from '../../../../../utils/mysql-queries'
-import { getTelegramUser } from '../../utils'
 
 const ADD_MYSELF = 'add_myself'
 
 export const mainState: TState<EState> = {
   stateName: EState.MAIN,
-  inlineMenu: async ctx => {
+  menu: async (ctx) => {
     const db = await getDbConnection()
     const userChannelsCount = await countPublisherChannelsByUserId(db, ctx.from.id)
     await db.close()
-    const buttons = [[Key.callback('Добавить канал', EState.ADD_CHANNEL)]]
+    const buttons: TMenuButton[][] = [[['Добавить канал', () => enterToState(ctx, addChannelState)]]]
     if (userChannelsCount !== 0) {
-      buttons.push([Key.callback('Настройки подписок', EState.CHANNEL_SELECT)])
+      buttons.push([['Настройки подписок', () => enterToState(ctx, channelSelectState)]])
     } else {
-      buttons.push([Key.callback('Добавить ключевые слова', ADD_MYSELF)])
+      buttons.push([['Добавить ключевые слова', async () => {
+        await onClickAddMyself(ctx)
+        await enterToState(ctx, addKeywordsState)
+      }]])
     }
-    return {
-      text: 'Меню подписок',
-      buttons,
-    }
+    return buttons
   },
-  onCallback: async (ctx, state) => {
-    if (state === ADD_MYSELF) {
-      const username = getTelegramUser(ctx.from, '')
-      ctx.session.channel = {
-        name: username.user,
-        id: ctx.from.id,
-        type: 'private',
-      }
-      const db = await getDbConnection()
-      const timestamp = Date.now() / 1000
-      await insertPublisherChannel(db, {
-        id: ctx.from.id,
-        userId: ctx.from.id,
-        username: username.user,
-        subscribers: 0,
-        timestamp,
-      })
-      await db.close()
-      await enterToState(ctx, addKeywordsState)
-    }
-    if (state === EState.ADD_CHANNEL) {
-      await enterToState(ctx, addChannelState)
-      return
-    }
-    if (state === EState.CHANNEL_SELECT) {
-      await enterToState(ctx, channelSelectState)
-      return
-    }
-    throw new InfoMessage(`Unknown menu state: ${state}`)
-  },
+  // inlineMenu: async ctx => {
+  //   const db = await getDbConnection()
+  //   const userChannelsCount = await countPublisherChannelsByUserId(db, ctx.from.id)
+  //   await db.close()
+  //   const buttons = [[Key.callback('Добавить канал', EState.ADD_CHANNEL)]]
+  //   if (userChannelsCount !== 0) {
+  //     buttons.push([Key.callback('Настройки подписок', EState.CHANNEL_SELECT)])
+  //   } else {
+  //     buttons.push([Key.callback('Добавить ключевые слова', ADD_MYSELF)])
+  //   }
+  //   return {
+  //     text: 'Меню подписок',
+  //     buttons,
+  //   }
+  // },
+  // onCallback: async (ctx, state) => {
+  //   if (state === ADD_MYSELF) {
+  //     await onClickAddMyself(ctx)
+  //     await enterToState(ctx, addKeywordsState)
+  //     return
+  //   }
+  //   if (state === EState.ADD_CHANNEL) {
+  //     await enterToState(ctx, addChannelState)
+  //     return
+  //   }
+  //   if (state === EState.CHANNEL_SELECT) {
+  //     await enterToState(ctx, channelSelectState)
+  //     return
+  //   }
+  //   throw new InfoMessage(`Unknown menu state: ${state}`)
+  // },
 }
--- services/servers/tg-bots/publisher/utils/enterToState.ts
+++ services/servers/tg-bots/publisher/utils/enterToState.ts
@@ -15,7 +15,25 @@ export const enterToState = async <GStateName extends EState>(
     }
     ctx.session.lastMenuId = menu.message_id
     const message = state.message?.(ctx)
-    if (message) ctx.reply(message)
-    return
+    if (message) await ctx.reply(message)
+  }
+  if (state.menu) {
+    const onTextOptions: Record<string, (ctx?: TTelegrafContext) => Promise<unknown> | unknown> = {}
+    const buttons = (await state.menu()).map(buttonRow => buttonRow.map(button => {
+      if (Array.isArray(button)) {
+        const [text, callback] = button
+        onTextOptions[text] = callback
+        return text
+      }
+      return button
+    }))
+    state.onText = async (ctx, text) => {
+      if (onTextOptions[text]) {
+        await onTextOptions[text](ctx)
+        return
+      }
+      await state.onText(ctx, text)
+    }
+    await ctx.reply('', Keyboard.make(buttons).reply())
   }
 }
