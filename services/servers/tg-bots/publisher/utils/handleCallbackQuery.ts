044e41bfe7bb12fa7b758e837d4a19b81ccd5ed8
Vishtar <viktor.kishchuk@gmail.com>
2024-04-10 00:09:03 +0400

    tg-publisher: fixes

--- services/servers/tg-bots/publisher/utils/handleCallbackQuery.ts
+++ services/servers/tg-bots/publisher/utils/handleCallbackQuery.ts
@@ -1,12 +1,11 @@
 import { Client } from '@elastic/elasticsearch'
 import { handleKeyAction, handleMemePost } from '.'
 import { TState, TTelegrafContext } from '../types'
-import { EState } from '../constants'
 
 export const handleCallbackQuery = async (
   ctx: TTelegrafContext,
   elastic: Client,
-  states: Record<EState, TState>,
+  handler: TState['onCallback'],
 ) => {
   // @ts-expect-error Property 'data' does not exist on type 'CallbackQuery'
   const callbackQuery = ctx.update.callback_query.data
@@ -19,5 +18,5 @@ export const handleCallbackQuery = async (
     await handleKeyAction(ctx, restCb[0], restCb[1])
     return
   }
-  await states[ctx.session.state].onCallback(ctx, callbackQuery)
+  await handler(ctx, callbackQuery)
 }
