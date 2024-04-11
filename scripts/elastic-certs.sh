62044550ffd08c3031e7a4966c832166da66305c
Vishtar <viktor.kishchuk@gmail.com>
2024-04-11 21:30:32 +0400

    scripts: elastic-serts version to args

--- scripts/elastic-certs.sh
+++ scripts/elastic-certs.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-docker run --rm -v $(pwd)/certs:/certs docker.elastic.co/elasticsearch/elasticsearch:8.11.3 \
-/bin/bash -c 'elasticsearch-certutil ca --out /certs/elastic-stack-ca.p12 --pass "" --silent'
+ELASTICSEARCH_VERSION=$1
 
-docker run --rm -v $(pwd)/certs:/certs docker.elastic.co/elasticsearch/elasticsearch:8.11.3 \
-/bin/bash -c 'elasticsearch-certutil cert --ca /certs/elastic-stack-ca.p12 --ca-pass "" --name memesearch-elastic --dns memesearch-elastic --ip 0.0.0.0 --out /certs/elastic-certificates.p12 --pass "" --silent'
+docker run --rm -v $(pwd)/certs:/certs docker.elastic.co/elasticsearch/elasticsearch:$ELASTICSEARCH_VERSION \
+bash -c 'elasticsearch-certutil ca --out /certs/elastic-stack-ca.p12 --pass "" --silent'
+
+docker run --rm -v $(pwd)/certs:/certs docker.elastic.co/elasticsearch/elasticsearch:$ELASTICSEARCH_VERSION \
+bash -c 'elasticsearch-certutil cert --ca /certs/elastic-stack-ca.p12 --ca-pass "" --name memesearch-elastic --dns memesearch-elastic --ip 0.0.0.0 --out /certs/elastic-certificates.p12 --pass "" --silent'
 
 openssl pkcs12 -in certs/elastic-stack-ca.p12 -clcerts -nokeys -out certs/elastic-stack-ca.pem -passin pass:
 openssl pkcs12 -in certs/elastic-certificates.p12 -nocerts -nodes -out certs/elastic-certificates.key -passin pass:
