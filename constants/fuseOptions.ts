e311edb095e6ba30a33215a85e1d21db1229a066
Vishtar <viktor.kishchuk@gmail.com>
2024-04-11 16:07:33 +0400

    tg-publisher: fuseOptions tune

--- constants/fuseOptions.ts
+++ constants/fuseOptions.ts
@@ -3,5 +3,6 @@ import { IFuseOptions } from "fuse.js"
 export const fuseOptions: IFuseOptions<string> = {
   isCaseSensitive: false,
   sortFn: (a, b) => b.score - a.score, // descending
-  threshold: .6,
+  threshold: .1,
+  ignoreLocation: true,
 }
