d2effd563a3066e9f24d19316ed6998ae787b6d7
Vishtar <viktor.kishchuk@gmail.com>
2024-04-12 15:38:03 +0400

    frontend: blacklist and minor fixes

---
+++ frontend/src/components/organisms/BlaclListSettings/style.ts
@@ -0,0 +1,9 @@
+import stylex from '@stylexjs/stylex'
+
+export const s = stylex.create({
+  blacklist: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '8px',
+  },
+})
--- services/ocr/utils/blackListChecker.ts
+++ services/ocr/utils/blackListChecker.ts
@@ -1,11 +1,17 @@
-import { getMysqlClient } from '../../../utils'
+import { getDbConnection } from '../../../utils'
 import { selectBlackList } from '../../../utils/mysql-queries'
 
 // return true if text doesn't contain blocked phrases
 export const blackListChecker = async (text: string) => {
-  const mysql = await getMysqlClient()
-  const blackList = await selectBlackList(mysql)
-  await mysql.end()
+  const db = await getDbConnection()
+  const blackListRows = await selectBlackList(db)
+  if (!blackListRows.length) {
+    throw new Error(
+      'There is no blacklist in the database. Please add them to the words cell from the words_blacklist table (one row, separated by lines)',
+    )
+  }
+  await db.close()
+  const blackList = blackListRows[0].words
   const regex = new RegExp(blackList.replace('\n', '|'), 'i')
 
   return !regex.test(text)
