ad64199bc4057983e158bad3229dd1da57b3dc35
Vishtar <viktor.kishchuk@gmail.com>
2024-04-16 13:32:27 +0400

    tg-publisher: matching for short words .startsWith(word)

--- utils/fuseSearch.ts
+++ utils/fuseSearch.ts
@@ -10,15 +10,22 @@ export const fuseSearch = (list: readonly string[], query: string) => {
   }
 
   let processedQuery = query
-  if (query.length >= 10 && /^[a-zА-Я]+$/i.test(query)) {
-    const trimmedQuery = removeLastSyllable(query)
-    if (trimmedQuery && trimmedQuery !== query) {
-      processedQuery = trimmedQuery
-      fuseOptions.threshold = 0
+  if (query.length >= 10) {
+    if (/^[a-zА-Я]+$/i.test(query)) {
+      const trimmedQuery = removeLastSyllable(query)
+      if (trimmedQuery && trimmedQuery !== query) {
+        processedQuery = trimmedQuery
+        fuseOptions.threshold = 0
+      }
     }
-  }
-  if (query.length <= 4) {
+  } else if (query.length <= 4) {
     fuseOptions.threshold = 0
+    const results = []
+    const [text] = list
+    const words = text.toLowerCase().split(' ')
+    const found = words.find(word => word.startsWith(query))
+    if (found) results.push({})
+    return results
   }
   const fuse = new Fuse(list, {
     ...fuseOptions,
