8292d95bdcbf55cab6ad168d77395f5eb4dbe413
Vishtar <viktor.kishchuk@gmail.com>
2024-04-17 14:35:31 +0400

    tg-pics: i18n.ru, start message changed

--- services/servers/tg-bots/pics/handlers/onBotCommandSuggestChannel.ts
+++ services/servers/tg-bots/pics/handlers/onBotCommandSuggestChannel.ts
@@ -1,21 +1,21 @@
 import { getMysqlClient, getTgChannelName, logError } from '../../../../../utils'
 import { insertChannelSuggestion } from '../../../../../utils/mysql-queries'
+import { i18n } from '../i18n'
 import { logUserAction } from '../utils'
 
 export const onBotCommandSuggestChannel = async (ctx, logger) => {
   const { payload } = ctx
   const channelName = getTgChannelName(payload.trim())
   if (!channelName)
-    return ctx.reply(`You must specify the channel name:
-/suggest_channel name`)
+    return ctx.reply(i18n['ru'].message.suggestionFormat)
   try {
     const mysql = await getMysqlClient()
     const response = await insertChannelSuggestion(mysql, channelName)
     await mysql.end()
     if (response) logUserAction(ctx.from, { info: `suggested @${channelName}` }, logger)
-    return ctx.reply('Thank you for the suggestion!')
+    return ctx.reply(i18n['ru'].message.suggestionThanks)
   } catch (e) {
     await logError(logger, e)
-    await ctx.reply('An error occurred, please try again later')
+    await ctx.reply(i18n['ru'].message.error)
   }
 }
