9872132d333459ebd2ebbec5f9818b598cf90ff1
Vishtar <viktor.kishchuk@gmail.com>
2024-04-19 12:41:44 +0400

    migrations

--- db/schema/botPublisherUsers.ts
+++ db/schema/botPublisherUsers.ts
@@ -1,7 +1,7 @@
-import { mysqlTable, int, varchar } from 'drizzle-orm/mysql-core'
+import { mysqlTable, int, varchar, bigint } from 'drizzle-orm/mysql-core'
 
 export const botPublisherUsers = mysqlTable('bot_publisher_users', {
-  id: int('id').primaryKey(),
+  id: bigint('id', { mode: 'number' }).primaryKey(),
   user: varchar('user', { length: 255 }).notNull(),
   keywordsCount: int('keywords_count'),
   timestamp: int('timestamp').notNull(),
