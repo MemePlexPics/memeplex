016ac1d901d1fc02952c8ece09c292e2544d0b6f
Vishtar <viktor.kishchuk@gmail.com>
2024-04-19 16:49:28 +0400

    tg-publisher: keywords settings state pagination

---
+++ utils/sqlWithPagination.ts
@@ -0,0 +1,10 @@
+import { MySqlSelect } from 'drizzle-orm/mysql-core'
+
+/** @param queryBuilder must have .orderBy() setted */
+export const sqlWithPagination = <TQueryBuilder extends MySqlSelect>(
+  queryBuilder: TQueryBuilder,
+  page: number,
+  pageSize: number,
+) => {
+  return queryBuilder.limit(pageSize).offset((page - 1) * pageSize)
+}
