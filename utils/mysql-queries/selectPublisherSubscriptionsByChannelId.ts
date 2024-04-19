240b85787f66e581aa624e73bf1da49ad41cd224
Vishtar <viktor.kishchuk@gmail.com>
2024-04-19 16:49:28 +0400

    tg-publisher: keywords settings state pagination

--- utils/mysql-queries/selectPublisherSubscriptionsByChannelId.ts
+++ utils/mysql-queries/selectPublisherSubscriptionsByChannelId.ts
@@ -2,12 +2,10 @@ import { botPublisherSubscriptions } from '../../db/schema'
 import { eq } from 'drizzle-orm'
 import { TDbConnection } from '../types'
 
-export const selectPublisherSubscriptionsByChannelId = async (
-  db: TDbConnection,
-  channelId: number,
-) => {
-  return await db
+export const selectPublisherSubscriptionsByChannelId = (db: TDbConnection, channelId: number) => {
+  return db
     .select({ keyword: botPublisherSubscriptions.keyword })
     .from(botPublisherSubscriptions)
     .where(eq(botPublisherSubscriptions.channelId, channelId))
+    .orderBy(botPublisherSubscriptions.keyword)
 }
