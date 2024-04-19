fbda83fab2a65d3a78acb4caa1ef72bb0de02149
Vishtar <viktor.kishchuk@gmail.com>
2024-04-19 12:41:44 +0400

    migrations

---
+++ db/schema/botPublisherKeywordGroups.ts
@@ -0,0 +1,6 @@
+import { mysqlTable, text, varchar } from 'drizzle-orm/mysql-core'
+
+export const botPublisherKeywordGroups = mysqlTable('bot_publisher_keyword_groups', {
+  name: varchar('name', { length: 255 }).primaryKey(),
+  keywords: text('keywords'),
+})
