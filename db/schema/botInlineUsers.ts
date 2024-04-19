276c12de6c27c7ad017fd1d596164b9d3f75b201
Vishtar <viktor.kishchuk@gmail.com>
2024-04-19 12:41:44 +0400

    migrations

--- db/schema/botInlineUsers.ts
+++ db/schema/botInlineUsers.ts
@@ -1,7 +1,7 @@
-import { mysqlTable, int, varchar } from 'drizzle-orm/mysql-core'
+import { mysqlTable, int, varchar, bigint } from 'drizzle-orm/mysql-core'
 
 export const botInlineUsers = mysqlTable('bot_inline_users', {
-  id: int('id').primaryKey(),
+  id: bigint('id', { mode: 'number' }).primaryKey(),
   user: varchar('user', { length: 255 }).notNull(),
   timestamp: int('timestamp').notNull(),
 })
