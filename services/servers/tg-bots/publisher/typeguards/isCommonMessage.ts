8d92340402f19abecef69bf99170e67cc8d12212
Vishtar <viktor.kishchuk@gmail.com>
2024-04-22 18:22:21 +0400

    tg-publisher: wip

--- services/servers/tg-bots/publisher/typeguards/isCommonMessage.ts
+++ services/servers/tg-bots/publisher/typeguards/isCommonMessage.ts
@@ -1,5 +1,5 @@
 import { Message } from 'telegraf/typings/core/types/typegram'
 
 export const isCommonMessage = (message: unknown): message is Message.CommonMessage => {
-  return 'callback_data' in (message as Message.CommonMessage)
+  return 'reply_markup' in (message as Message.CommonMessage)
 }
