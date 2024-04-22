d6f88781cfc6a124f196fac0b6bd81e3c724c418
Vishtar <viktor.kishchuk@gmail.com>
2024-04-22 13:55:09 +0400

    tg-publisher: wip

--- services/servers/tg-bots/publisher/i18n/ru.ts
+++ services/servers/tg-bots/publisher/i18n/ru.ts
@@ -1,5 +1,7 @@
 export const ru = {
   message: {
-    channelUnlinked: 'Канал успешно отвязан',
+    channelUnlinked: 'Канал успешно отвязан.',
+    memePostedSuccessfully: 'Мем успешно опубликован.',
+    adminRightForPost: 'Для публикации мема в канал необходмо предоставить боту админ-права на отправку сообщений в данный канал.',
   },
 }
