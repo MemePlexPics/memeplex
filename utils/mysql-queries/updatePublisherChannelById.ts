d6826445eea669a82c3487aca6d756e746622009
Vishtar <viktor.kishchuk@gmail.com>
2024-04-22 13:00:52 +0400

    tg-publisher: wip

---
+++ utils/mysql-queries/updatePublisherChannelById.ts
@@ -0,0 +1,14 @@
+import { eq } from 'drizzle-orm'
+import { botPublisherChannels } from '../../db/schema'
+import { TDbConnection } from '../types'
+
+export const updatePublisherChannelById = async (
+  db: TDbConnection,
+  values: Partial<Omit<typeof botPublisherChannels.$inferInsert, 'id' | 'timestamp'>>,
+  id: number
+) => {
+  await db
+    .update(botPublisherChannels)
+    .set(values)
+    .where(eq(botPublisherChannels.id, id))
+}
