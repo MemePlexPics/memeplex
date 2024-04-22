32f550ce477ddf16c9d3a4b5b71ed4c243cf3701
Vishtar <viktor.kishchuk@gmail.com>
2024-04-22 16:06:10 +0400

    services: js -> ts

---
+++ services/check-proxies/utils/handleAddingProxy.ts
@@ -0,0 +1,24 @@
+import { findExistedProxy, insertProxy } from '../../../utils/mysql-queries'
+import { checkProxy } from '.'
+
+export const handleAddingProxy = async (
+  mysql,
+  proxy,
+  ipWithoutProxy,
+  logger,
+) => {
+  const proxyString = `${proxy.ip}:${proxy.port}`
+  const found = await findExistedProxy(mysql, proxyString, proxy.protocol)
+  if (!found) {
+    const result = await checkProxy(proxy, ipWithoutProxy, logger)
+    await insertProxy(
+      mysql,
+      `${proxy.ip}:${proxy.port}`,
+      proxy.protocol,
+      result.availability,
+      result.anonymity,
+      result.speed,
+      result.lastCheckDatetime,
+    )
+  }
+}
