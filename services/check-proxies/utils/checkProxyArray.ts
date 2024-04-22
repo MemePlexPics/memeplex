a93554876f09a09d425e89e9fded8bc80d1522ba
Vishtar <viktor.kishchuk@gmail.com>
2024-04-22 17:52:37 +0400

    tg-publisher: wip

--- services/check-proxies/utils/checkProxyArray.ts
+++ services/check-proxies/utils/checkProxyArray.ts
@@ -1,12 +1,7 @@
 import { checkProxy } from '../utils'
 import { updateProxy } from '../../../utils/mysql-queries'
 
-export const checkProxyArray = async (
-  mysql,
-  proxies,
-  ipWithoutProxy,
-  logger,
-) => {
+export const checkProxyArray = async (mysql, proxies, ipWithoutProxy, logger) => {
   for (const proxy of proxies) {
     const result = await checkProxy(proxy, ipWithoutProxy, logger)
     await updateProxy(
--- utils/getTgChannelName.ts
+++ utils/getTgChannelName.ts
@@ -1,1 +1,3 @@
-export const getTgChannelName = (link: string) => link.replace('https://t.me/', '').replace('@', '')
+// TODO: /[a-zA-Z_0-9]+/.match(link).at(-1)
+export const getTgChannelName = (link: string) =>
+  link.replace('https://t.me/', '').replace('/', '').replace('@', '')
