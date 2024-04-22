34f14500b5db4e71597c71e91c7c35140922550c
Vishtar <viktor.kishchuk@gmail.com>
2024-04-22 18:22:21 +0400

    tg-publisher: wip

--- services/servers/tg-bots/publisher/typeguards/isCallbackButton.ts
+++ services/servers/tg-bots/publisher/typeguards/isCallbackButton.ts
@@ -3,5 +3,5 @@ import { InlineKeyboardButton } from 'telegraf/typings/core/types/typegram'
 export const isCallbackButton = (
   button: unknown,
 ): button is InlineKeyboardButton.CallbackButton => {
-  return 'reply_markup' in (button as InlineKeyboardButton.CallbackButton)
+  return 'callback_data' in (button as InlineKeyboardButton.CallbackButton)
 }
