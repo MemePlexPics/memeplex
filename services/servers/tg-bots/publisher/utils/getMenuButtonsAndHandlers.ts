461deeea8c6c1992d1168c9c676e590d7ac16757
Vishtar <viktor.kishchuk@gmail.com>
2024-04-22 14:10:51 +0400

    tg-publisher: handle meme post error cause by not admin bot

--- services/servers/tg-bots/publisher/utils/getMenuButtonsAndHandlers.ts
+++ services/servers/tg-bots/publisher/utils/getMenuButtonsAndHandlers.ts
@@ -3,8 +3,8 @@ import { TState, TTelegrafContext } from '../types'
 
 export const getMenuButtonsAndHandlers = async (ctx: TTelegrafContext, state: TState) => {
   const onTextHandlers: Record<
-    string,
-    (ctx?: TTelegrafContext, text?: string) => Promisable<unknown>
+  string,
+  (ctx?: TTelegrafContext, text?: string) => Promisable<unknown>
   > = {}
   const { text, buttons: buttonsRaw } = await state.menu(ctx)
   const buttons = buttonsRaw.map(buttonRow =>
