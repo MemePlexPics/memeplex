5aedb43a1e16e46a686581262ad58c4e46f1a2f9
Vishtar <viktor.kishchuk@gmail.com>
2024-04-22 13:55:09 +0400

    tg-publisher: wip

--- utils/mysql-queries/proceedChannelSuggestion.ts
+++ utils/mysql-queries/proceedChannelSuggestion.ts
@@ -2,10 +2,7 @@ import { eq } from 'drizzle-orm'
 import { channelSuggestions } from '../../db/schema'
 import { TDbConnection } from '../types'
 
-export const proceedChannelSuggestion = async (
-  db: TDbConnection,
-  channel: string,
-) => {
+export const proceedChannelSuggestion = async (db: TDbConnection, channel: string) => {
   await db
     .update(channelSuggestions)
     .set({ processed: true })
