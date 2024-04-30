9b047634e5cc06e92d59812d633458aa4db6eb83
Vishtar <viktor.kishchuk@gmail.com>
2024-05-01 00:04:59 +0400

    tg-publisher: fix

---
+++ utils/mysql-queries/deletePublisherGroupSubscriptionByChannelId.ts
@@ -0,0 +1,14 @@
+import { botPublisherGroupSubscriptions } from '../../db/schema'
+import { eq } from 'drizzle-orm'
+import { TDbConnection } from '../types'
+
+export const deletePublisherGroupSubscriptionByChannelId = async (
+  db: TDbConnection,
+  channelId: number,
+) => {
+  await db
+    .delete(botPublisherGroupSubscriptions)
+    .where(
+      eq(botPublisherGroupSubscriptions.channelId, channelId),
+    )
+}
