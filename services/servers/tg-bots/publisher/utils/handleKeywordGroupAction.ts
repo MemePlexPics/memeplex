2aacd9650cedcfa83ca3ec91750839b504bba026
Vishtar <viktor.kishchuk@gmail.com>
2024-05-01 16:40:50 +0400

    tg-publisher: refactoring, text changes

---
+++ services/servers/tg-bots/publisher/utils/handleKeywordGroupAction.ts
@@ -0,0 +1,45 @@
+import { logUserAction } from '.'
+import { getDbConnection } from '../../../../../utils'
+import {
+  deletePublisherGroupSubscription,
+  selectPublisherChannelsByUserId,
+} from '../../../../../utils/mysql-queries'
+import { ECallback, EKeywordAction } from '../constants'
+import { isCallbackButton, isCommonMessage } from '../typeguards'
+import { TTelegrafContext } from '../types'
+
+export const handleKeywordGroupAction = async (
+  ctx: TTelegrafContext,
+  command: EKeywordAction,
+  keywordGroup: string,
+) => {
+  if (command === EKeywordAction.DELETE) {
+    const db = await getDbConnection()
+    const userChannels = await selectPublisherChannelsByUserId(db, ctx.from.id)
+    for (const channel of userChannels) {
+      await deletePublisherGroupSubscription(db, channel.id, keywordGroup)
+    }
+    await db.close()
+    if (isCommonMessage(ctx.callbackQuery.message)) {
+      await ctx.editMessageReplyMarkup({
+        inline_keyboard: ctx.callbackQuery.message.reply_markup.inline_keyboard.map(row =>
+          row.filter(
+            column =>
+              !isCallbackButton(column) ||
+              column.callback_data !==
+                `${ECallback.GROUP}|${EKeywordAction.DELETE}|${keywordGroup}`,
+          ),
+        ),
+      })
+    }
+    await ctx.reply(`Группа ключевых слов «${keywordGroup}» успешно удалена.`, {
+      reply_parameters: {
+        message_id: ctx.callbackQuery.message.message_id,
+      },
+    })
+    logUserAction(ctx.from, {
+      info: `Unsubscribe from a keyword group`,
+      keywordGroup,
+    })
+  }
+}
