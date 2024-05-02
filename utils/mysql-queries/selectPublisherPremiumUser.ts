a1bf7d26ff4b4ef2addbd175294c459b9e596b92
Vishtar <viktor.kishchuk@gmail.com>
2024-05-02 19:58:00 +0400

    tg-publisher: fix

--- utils/mysql-queries/selectPublisherPremiumUser.ts
+++ utils/mysql-queries/selectPublisherPremiumUser.ts
@@ -1,5 +1,5 @@
 import { botPublisherUserPremiums } from '../../db/schema'
-import { and, eq, lte } from 'drizzle-orm'
+import { and, eq, gte } from 'drizzle-orm'
 import { TDbConnection } from '../types'
 
 export const selectPublisherPremiumUser = async (db: TDbConnection, id: number) => {
@@ -9,7 +9,7 @@ export const selectPublisherPremiumUser = async (db: TDbConnection, id: number) 
     .where(
       and(
         eq(botPublisherUserPremiums.userId, id),
-        lte(botPublisherUserPremiums.untilTimestamp, Date.now() / 1000),
+        gte(botPublisherUserPremiums.untilTimestamp, Date.now() / 1000),
       ),
     )
 }
