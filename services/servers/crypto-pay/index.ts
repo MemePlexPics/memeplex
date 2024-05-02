1f6f191f76c1d1687f9c8ebc6b1896364705560e
Vishtar <viktor.kishchuk@gmail.com>
2024-05-02 19:45:19 +0400

    tg-publisher: wip

--- services/servers/crypto-pay/index.ts
+++ services/servers/crypto-pay/index.ts
@@ -3,9 +3,10 @@ import 'dotenv/config'
 import { CryptoPay } from '@foile/crypto-pay-api'
 import { ECryptoPayHostname } from './constants'
 import { loopRetrying } from '../../../utils'
-import { handleInvoiceCreation, handlePaidInvoice } from './utils'
+import { handleInvoiceCreation, handleMissedPaidInvoices, handlePaidInvoice } from './utils'
 import { getLogger } from '../tg-bots/utils'
 import { CYCLE_SLEEP_TIMEOUT, LOOP_RETRYING_DELAY } from '../../../constants'
+import { TInvoice } from './types'
 
 const logger = getLogger('cryptopay-bot')
 
@@ -18,10 +19,23 @@ const cryptoPay: CryptoPay = new CryptoPay(process.env.CRYPTOPAY_BOT_TEST_TOKEN,
   },
 })
 
-cryptoPay.on('invoice_paid', handlePaidInvoice)
+cryptoPay.on('invoice_paid', async (update: { payload: TInvoice }) => {
+  const userId = update.payload.description?.match(/\((.+)\)/)?.at(-1)
+  if (!userId) {
+    throw new Error(
+      `There is no userId in an invoice description: ${JSON.stringify(update.payload)}`,
+    )
+  }
+  await handlePaidInvoice(Number(userId), update.payload.invoice_id)
+})
 
 const main = async () => {
-  await loopRetrying(() => handleInvoiceCreation(cryptoPay, logger), {
+  loopRetrying(() => handleInvoiceCreation(cryptoPay, logger), {
+    logger,
+    afterCallbackDelayMs: CYCLE_SLEEP_TIMEOUT,
+    catchDelayMs: LOOP_RETRYING_DELAY,
+  })
+  loopRetrying(() => handleMissedPaidInvoices(cryptoPay), {
     logger,
     afterCallbackDelayMs: CYCLE_SLEEP_TIMEOUT,
     catchDelayMs: LOOP_RETRYING_DELAY,
