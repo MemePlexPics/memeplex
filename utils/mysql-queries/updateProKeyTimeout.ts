0261a8edee06328d439340c8e9317e43afbb0268
Vishtar <viktor.kishchuk@gmail.com>
2024-05-02 16:49:19 +0400

    memesearch: fix

--- utils/mysql-queries/updateProKeyTimeout.ts
+++ utils/mysql-queries/updateProKeyTimeout.ts
@@ -1,7 +1,7 @@
 import { eq } from 'drizzle-orm'
-import { ocrKeys, ocrKeysPro } from '../../db/schema'
+import { ocrKeysPro } from '../../db/schema'
 import { TDbConnection } from '../types'
 
 export async function updateProKeyTimeout(db: TDbConnection, key: string, timeout: string) {
-  await db.update(ocrKeysPro).set({ timeout }).where(eq(ocrKeys.ocrKey, key))
+  await db.update(ocrKeysPro).set({ timeout }).where(eq(ocrKeysPro.ocrKey, key))
 }
