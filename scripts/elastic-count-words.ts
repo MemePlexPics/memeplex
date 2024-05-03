12c7bae5bcdfc1c18228817441a3384ca4aa96d2
Vishtar <viktor.kishchuk@gmail.com>
2024-05-03 15:58:30 +0400

    scripts: elastic-count-words.ts

---
+++ scripts/elastic-count-words.ts
@@ -0,0 +1,62 @@
+import fs from 'fs'
+import { getElasticClient } from '../utils'
+import { ELASTIC_INDEX } from '../constants'
+import { Client } from '@elastic/elasticsearch'
+
+const countEntities = async (client: Client, words: string[]) => {
+  const query = {
+    query: {
+      bool: {
+        should: words.map(word => ({
+          match: {
+            eng: word,
+          }
+        })),
+        minimum_should_match: 1
+      }
+    }
+  }
+
+  const response = await client.count({
+    index: ELASTIC_INDEX,
+    body: query,
+  })
+
+  return response.count
+}
+
+const processMultilineString = async (multilineString: string, client: Client) => {
+  const result: { [key: string]: number } = {}
+  const lines = multilineString.split('\n')
+
+  for (const line of lines) {
+    if (line.trim()) {
+      const words = line.split(/,\s*/)
+      const count = await countEntities(client, words)
+      result[line] = count
+    }
+  }
+
+  return result
+}
+
+const saveResultToFile = (result: { [key: string]: number }, filePath: string) => {
+  const json = JSON.stringify(result, null, 2)
+  fs.writeFileSync(filePath, json, 'utf-8')
+}
+
+const main =  async () => {
+  const client = await getElasticClient()
+  const words = `
+Москва, moscow
+питер, петербург, ленинград
+Новосибирск, новосиб`
+
+  const result = await processMultilineString(words, client)
+
+  const jsonFilePath = './counted-words.json'
+  saveResultToFile(result, jsonFilePath)
+  console.log(`Results saved to ${jsonFilePath}`)
+}
+
+main()
