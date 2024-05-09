3e7914e6f788aa357aa88a23aa805dd31753b639
Vishtar <viktor.kishchuk@gmail.com>
2024-05-09 13:24:06 +0400

    grafana: new queues on the panel

--- services/promtail-workers/amqpQueueMessageCounter.ts
+++ services/promtail-workers/amqpQueueMessageCounter.ts
@@ -2,29 +2,19 @@ import amqplib from 'amqplib'
 import process from 'process'
 import 'dotenv/config'
 
-import {
-  AMQP_IMAGE_DATA_CHANNEL,
-  AMQP_IMAGE_FILE_CHANNEL,
-  AMQP_CHECK_PROXY_CHANNEL,
-  AMQP_PUBLISHER_DISTRIBUTION_CHANNEL,
-} from '../../constants'
+import * as queues from '../../constants/amqpChannels'
 import { sendToLoki } from '../../utils'
 import { Logger } from 'winston'
 
 export const amqpQueueMessageCounter = async (_logger: Logger) => {
-  const queues = [
-    AMQP_IMAGE_DATA_CHANNEL,
-    AMQP_IMAGE_FILE_CHANNEL,
-    AMQP_CHECK_PROXY_CHANNEL,
-    AMQP_PUBLISHER_DISTRIBUTION_CHANNEL,
-  ]
   const amqp = await amqplib.connect(process.env.AMQP_ENDPOINT)
-  const lokiMessage = {}
-  for (const queue of queues) {
+  const lokiMessage: Record<string, number> = {}
+  for (const queue in queues) {
+    const queueName = queues[queue]
     const channel = await amqp.createChannel()
     const { messageCount } = await channel.assertQueue(queue)
     await channel.close()
-    lokiMessage[queue] = messageCount
+    lokiMessage[queueName] = messageCount
   }
   await sendToLoki({ amqp: 'queue' }, [JSON.stringify(lokiMessage)])
 }
