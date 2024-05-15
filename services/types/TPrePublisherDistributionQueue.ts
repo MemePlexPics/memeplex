1a7e9ef2e0f0a90bacb840fa1b2d08b74c35dbf1
Vishtar <viktor.kishchuk@gmail.com>
2024-05-15 20:35:50 +0400

    publisher: fix

--- services/types/TPrePublisherDistributionQueue.ts
+++ services/types/TPrePublisherDistributionQueue.ts
@@ -2,5 +2,13 @@ import { TPublisherDistributionQueueMsg } from '.'
 
 export type TPrePublisherDistributionQueue = Record<
 number,
-Omit<TPublisherDistributionQueueMsg, 'userId' | 'keywords' | 'keywordGroups' | 'groupKeywords' | 'channelIdsByKeyword' | 'channelIdsByKeywordGroup'>
+Omit<
+TPublisherDistributionQueueMsg,
+| 'userId'
+| 'keywords'
+| 'keywordGroups'
+| 'groupKeywords'
+| 'channelIdsByKeyword'
+| 'channelIdsByKeywordGroup'
+>
 >
