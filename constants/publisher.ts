d89dbe16fafe3f2390dbd3e1fe25cfdbcbe3fa10
Vishtar <viktor.kishchuk@gmail.com>
2024-05-15 20:35:50 +0400

    publisher: fix

--- constants/publisher.ts
+++ constants/publisher.ts
@@ -3,13 +3,16 @@ export const PREMIUM_PLANS = [
   {
     months: 1,
     cost: 1,
+    emoji: '⭐',
   },
   {
     months: 3,
     cost: 3,
+    emoji: '✨',
   },
   {
     months: 12,
     cost: 10,
+    emoji: '🌟',
   },
 ] as const
