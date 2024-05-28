b094e895fc505a0791d14bca724999de8869ae90
Vishtar <viktor.kishchuk@gmail.com>
2024-05-29 01:34:19 +0400

    search.js -> .ts

---
+++ scripts/search.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import * as readline from 'readline'
+import process from 'process'
+
+import { getElasticClient } from '../utils/index'
+import { ELASTIC_PAGE_SIZE } from '../constants/index'
+import { searchMemes } from '../services/servers/utils/index'
+
+const client = await getElasticClient()
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+})
+
+rl.on('line', async (line) => {
+  const res = await searchMemes(client, line, 1, ELASTIC_PAGE_SIZE)
+  console.log(res)
+})
+
+rl.once('close', () => {
+  console.log('bye!')
+})
