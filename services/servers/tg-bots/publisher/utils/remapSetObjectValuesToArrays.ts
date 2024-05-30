6df895b78c6edec3dc5123d354d714c46201a6db
Vishtar <viktor.kishchuk@gmail.com>
2024-05-30 23:56:45 +0400

    scripts: setBotTopics

--- services/servers/tg-bots/publisher/utils/remapSetObjectValuesToArrays.ts
+++ services/servers/tg-bots/publisher/utils/remapSetObjectValuesToArrays.ts
@@ -1,9 +1,10 @@
 export const remapSetObjectValuesToArrays = <GSetElement>(
   objectWithSet: Record<string, Set<GSetElement>>,
-) => Object.entries(objectWithSet).reduce<Record<string, GSetElement[]>>(
-  (acc, [keyword, channelIds]) => {
-    acc[keyword] = [...channelIds]
-    return acc
-  },
-  {},
-)
+) =>
+  Object.entries(objectWithSet).reduce<Record<string, GSetElement[]>>(
+    (acc, [keyword, channelIds]) => {
+      acc[keyword] = [...channelIds]
+      return acc
+    },
+    {},
+  )
