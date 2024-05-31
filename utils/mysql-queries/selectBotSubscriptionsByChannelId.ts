6a434ee2e8114ffd2051b75382ef77ae3a2ef15b
Vishtar <viktor.kishchuk@gmail.com>
2024-05-31 18:29:22 +0400

    publisher: fix slow join (I hope)

--- utils/mysql-queries/selectBotSubscriptionsByChannelId.ts
+++ utils/mysql-queries/selectBotSubscriptionsByChannelId.ts
@@ -1,32 +1,15 @@
-import {
-  botKeywords,
-  botSubscriptions,
-  botTopicNames,
-  botTopicSubscriptions,
-  botTopics,
-} from '../../db/schema'
-import { eq, or } from 'drizzle-orm'
+import { botKeywords, botSubscriptions } from '../../db/schema'
+import { eq } from 'drizzle-orm'
 import type { TDbConnection } from '../types'
 
 export const selectBotSubscriptionsByChannelId = (db: TDbConnection, channelId: number) => {
   return db
     .select({
       keyword: botKeywords.keyword,
-      keywordId: botKeywords.id,
-      topic: botTopicNames.name,
-      topicId: botTopicNames.id,
+      keywordId: botSubscriptions.keywordId,
     })
     .from(botSubscriptions)
-    .leftJoin(botTopicSubscriptions, eq(botTopicSubscriptions.channelId, channelId))
-    .leftJoin(botTopicNames, eq(botTopicNames.id, botTopicSubscriptions.topicId))
-    .leftJoin(botTopics, eq(botTopics.nameId, botTopicNames.id))
-    .leftJoin(botKeywords, or(
-      eq(botSubscriptions.keywordId, botKeywords.id),
-      eq(botTopics.keywordId, botKeywords.id),
-    ))
-    .where(or(
-      eq(botSubscriptions.channelId, channelId),
-      eq(botTopicSubscriptions.channelId, channelId)
-    ))
+    .leftJoin(botKeywords, eq(botSubscriptions.keywordId, botKeywords.id))
+    .where(eq(botSubscriptions.channelId, channelId))
     .orderBy(botSubscriptions.keywordId)
 }
