f2b9f049fcc16e9b42744415792d97b73de0e81f
Vishtar <viktor.kishchuk@gmail.com>
2024-05-31 14:41:01 +0400

    publisher: /set_premium admin comand

--- utils/mysql-queries/selectBotTopicIdSubscriptionsByChannelId.ts
+++ utils/mysql-queries/selectBotTopicIdSubscriptionsByChannelId.ts
@@ -2,7 +2,10 @@ import { botTopicSubscriptions } from '../../db/schema'
 import { eq } from 'drizzle-orm'
 import type { TDbConnection } from '../types'
 
-export const selectBotTopicIdSubscriptionsByChannelId = async (db: TDbConnection, channelId: number) => {
+export const selectBotTopicIdSubscriptionsByChannelId = async (
+  db: TDbConnection,
+  channelId: number,
+) => {
   return await db
     .select({
       topicId: botTopicSubscriptions.topicId,
