8a91ff6c1c89c63fa5144b99327691ea79ab95e4
Vishtar <viktor.kishchuk@gmail.com>
2024-06-04 19:38:34 +0400

    memesearch: mysql query minutes -> microseconds

--- utils/mysql-queries/updateProKeyTimeout.ts
+++ utils/mysql-queries/updateProKeyTimeout.ts
@@ -4,5 +4,5 @@ import type { TDbConnection } from '../types'
 import { OCR_SPACE_403_DELAY } from '../../constants'
 
 export async function updateProKeyTimeout(db: TDbConnection, key: string) {
-  await db.update(ocrKeysPro).set({ timeout: sql`DATE_ADD(NOW(), INTERVAL ${OCR_SPACE_403_DELAY}000 MINUTE)` }).where(eq(ocrKeysPro.ocrKey, key))
+  await db.update(ocrKeysPro).set({ timeout: sql`DATE_ADD(NOW(), INTERVAL ${OCR_SPACE_403_DELAY}000 MICROSECOND)` }).where(eq(ocrKeysPro.ocrKey, key))
 }
