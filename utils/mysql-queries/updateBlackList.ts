6059efa78a8fb14f541a2d85b25673a5928bae5c
Vishtar <viktor.kishchuk@gmail.com>
2024-06-10 13:44:11 +0400

    publisher: fix handler meme posts without admin rights

--- utils/mysql-queries/updateBlackList.ts
+++ utils/mysql-queries/updateBlackList.ts
@@ -5,5 +5,6 @@ export const updateBlackList = async (
   db: TDbConnection,
   words: typeof wordsBlacklist.$inferInsert,
 ) => {
+  // eslint-disable-next-line drizzle/enforce-update-with-where
   return await db.update(wordsBlacklist).set(words)
 }
