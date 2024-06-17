0bf59f97b8b9194686b8eefbe0b32fb0454da681
Vishtar <viktor.kishchuk@gmail.com>
2024-06-17 19:32:12 +0400

    frontend: prettier, eslint

--- frontend/src/components/atoms/Select/style.ts
+++ frontend/src/components/atoms/Select/style.ts
@@ -5,13 +5,13 @@ import { color, size } from '@/styles/variables.stylex'
 export const s = stylex.create({
   select: {
     backgroundColor: color.black_1,
-    borderWidth: '1px',
-    borderStyle: 'solid',
     borderColor: color.lightGray,
     borderRadius: '4px',
+    borderStyle: 'solid',
+    borderWidth: '1px',
     color: color.white,
     fontSize: size.fontNormal,
-    width: '100%',
     padding: '4px',
+    width: '100%',
   },
 })
