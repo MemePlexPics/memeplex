7cbea288cf0c030f51e280a1eff3677c61a30b68
Vishtar <viktor.kishchuk@gmail.com>
2024-06-17 20:00:15 +0400

    frontend: notification "There are X more memes"

--- frontend/src/pages/Home/hooks/constants/pageOptionsDefault.ts
+++ frontend/src/pages/Home/hooks/constants/pageOptionsDefault.ts
@@ -3,6 +3,7 @@ import { IPageOptions } from '../../types'
 export const pageOptionsDefault: IPageOptions = {
   query: '',
   currentPage: 1,
+  total: 0,
   totalPages: 0,
   from: undefined,
   to: undefined,
--- frontend/src/pages/Home/types/IPageOptions.ts
+++ frontend/src/pages/Home/types/IPageOptions.ts
@@ -1,6 +1,7 @@
 export type IPageOptions = {
   query: string
   currentPage: number
+  total: number
   totalPages: number
   from?: number
   to?: number
