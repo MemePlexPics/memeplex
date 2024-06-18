2fad5e7899678bf35ee06822f024f11b6767a244
Vishtar <viktor.kishchuk@gmail.com>
2024-06-18 18:06:24 +0400

    pics: meme suggestion fix

--- services/servers/tg-bots/pics/utils/onPhotoMessage.ts
+++ services/servers/tg-bots/pics/utils/onPhotoMessage.ts
@@ -6,22 +6,6 @@ import type { Message, Update } from 'telegraf/typings/core/types/typegram'
 export const onPhotoMessage = async (
   ctx: TTelegrafContext<Update.MessageUpdate<Message.PhotoMessage>>,
 ) => {
-  const { caption, photo } = ctx.update.message
-  if (!caption) {
-    for (const photoEntity of photo) {
-      const acceptMemeButton = Markup.button.callback(
-        'Опубликовать',
-        `${EMemeSuggestionCallback.PHOTO}|${photoEntity.file_id}`,
-      )
-      await ctx.telegram.sendPhoto(chatIds.premoderation, photoEntity.file_id, {
-        reply_markup: {
-          inline_keyboard: [[acceptMemeButton]],
-        },
-      })
-    }
-    return
-  }
-
   const forwardedMessage = await ctx.forwardMessage(chatIds.premoderation)
   await ctx.telegram.sendMessage(chatIds.premoderation, `Ololo`, {
     reply_parameters: {
