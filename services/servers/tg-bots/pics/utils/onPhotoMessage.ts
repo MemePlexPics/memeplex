672e694cb0bd6a02fd6380b5aa7aac54a82a8a40
Vishtar <viktor.kishchuk@gmail.com>
2024-06-18 19:09:59 +0400

    pics: store meme suggestions in DB

--- services/servers/tg-bots/pics/utils/onPhotoMessage.ts
+++ services/servers/tg-bots/pics/utils/onPhotoMessage.ts
@@ -2,24 +2,40 @@ import { Markup } from 'telegraf'
 import { EMemeSuggestionCallback, chatIds } from '../constants'
 import type { TTelegrafContext } from '../types'
 import type { Message, Update } from 'telegraf/typings/core/types/typegram'
+import { getDbConnection } from '../../../../../utils'
+import { botMemeSuggestions } from '../../../../../db/schema'
+import { eq } from 'drizzle-orm'
 
 export const onPhotoMessage = async (
   ctx: TTelegrafContext<Update.MessageUpdate<Message.PhotoMessage>>,
 ) => {
-  const forwardedMessage = await ctx.forwardMessage(chatIds.premoderation)
-  await ctx.telegram.sendMessage(chatIds.premoderation, `Ololo`, {
-    reply_parameters: {
-      message_id: forwardedMessage.message_id,
-    },
-    reply_markup: {
-      inline_keyboard: [
-        [
-          Markup.button.callback(
-            'Опубликовать',
-            `${EMemeSuggestionCallback.MESSAGE}|${forwardedMessage.message_id}`,
-          ),
-        ],
-      ],
-    },
-  })
+  const { caption, photo } = ctx.update.message
+  const db = await getDbConnection()
+  for (const photoEntity of photo) {
+    // TODO: move it out
+    await db.insert(botMemeSuggestions).ignore().values({
+      userId: ctx.from.id,
+      fileId: photoEntity.file_id,
+      text: caption,
+    })
+    const [suggestedMeme] = await db
+      .select({ id: botMemeSuggestions.id })
+      .from(botMemeSuggestions)
+      .where(eq(botMemeSuggestions.fileId, photoEntity.file_id))
+    await db.close()
+    const approveMemeButton = Markup.button.callback(
+      '👍 Опубликовать',
+      `${EMemeSuggestionCallback.APPROVE}|${suggestedMeme.id}`,
+    )
+    const declineMemeButton = Markup.button.callback(
+      '👎 Отклонить',
+      `${EMemeSuggestionCallback.DECLINE}|${suggestedMeme.id}`,
+    )
+    await ctx.telegram.sendPhoto(chatIds.premoderation, photoEntity.file_id, {
+      caption,
+      reply_markup: {
+        inline_keyboard: [[approveMemeButton, declineMemeButton]],
+      },
+    })
+  }
 }
