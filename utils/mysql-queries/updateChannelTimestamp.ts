dcc1e91d21c031309f067ec1be35de346bf699ce
Vishtar <viktor.kishchuk@gmail.com>
2024-06-18 16:42:50 +0400

    tsconfig: "strict" is true

--- utils/mysql-queries/updateChannelTimestamp.ts
+++ utils/mysql-queries/updateChannelTimestamp.ts
@@ -1,10 +1,12 @@
-export async function updateChannelTimestamp(mysql, name, timestamp) {
-  await mysql.query(
-    `
-        UPDATE channels
-        SET timestamp = ?
-        WHERE name = ?
-    `,
-    [timestamp, name],
-  )
+import { eq } from 'drizzle-orm'
+import { channels } from '../../db/schema'
+import type { TDbConnection } from '../types'
+
+export async function updateChannelTimestamp(db: TDbConnection, name: string, timestamp: number) {
+  await db
+    .update(channels)
+    .set({
+      timestamp,
+    })
+    .where(eq(channels.name, name))
 }
