d707b3434f65c8954ab7046439f567c9114e9a6e
Vishtar <viktor.kishchuk@gmail.com>
2024-06-18 19:09:59 +0400

    pics: store meme suggestions in DB

---
+++ db/schema/botMemeSuggestions.ts
@@ -0,0 +1,11 @@
+import { mysqlTable, int, varchar, text, bigint } from 'drizzle-orm/mysql-core'
+import { botUsers } from '.'
+
+export const botMemeSuggestions = mysqlTable('bot_meme_suggestions', {
+  id: int('id').autoincrement().primaryKey(),
+  userId: bigint('user_id', { mode: 'number' })
+    .notNull()
+    .references(() => botUsers.id),
+  fileId: varchar('file_id', { length: 255 }).unique().notNull(),
+  text: text('text'),
+})
