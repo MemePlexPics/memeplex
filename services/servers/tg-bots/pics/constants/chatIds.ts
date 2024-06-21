1be9807a6577871156c9b6f9a468d7b77222b5c8
Vishtar <viktor.kishchuk@gmail.com>
2024-06-21 15:03:05 +0400

    pics: notify about indexed suggested meme

--- services/servers/tg-bots/pics/constants/chatIds.ts
+++
@@ -1,4 +0,0 @@
-export const chatIds: Record<string, number | `@${string}`> = {
-  premoderation: -4174491694,
-  memes: '@memeplex_best',
-}
--- services/servers/tg-bots/pics/utils/handleSuggestedMemePremoderation.ts
+++ services/servers/tg-bots/pics/utils/handleSuggestedMemePremoderation.ts
@@ -1,16 +1,12 @@
 import type { CallbackQuery, Update } from 'telegraf/typings/core/types/typegram'
 import type { TTelegrafContext } from '../types'
-import {
-  ECallback,
-  EMemeSuggestionCallback,
-  chatIds,
-  memeSuggestionStatusByAction,
-} from '../constants'
+import { ECallback, EMemeSuggestionCallback, memeSuggestionStatusByAction } from '../constants'
 import { Markup } from 'telegraf'
 import { getTelegramUser } from '../../utils'
 import { getDbConnection } from '../../../../../utils'
 import { botMemeSuggestions } from '../../../../../db/schema'
 import { eq } from 'drizzle-orm'
+import { telegramChat } from '../../../../../constants'
 
 export const handleSuggestedMemePremoderation = async <GAction extends EMemeSuggestionCallback>(
   ctx: TTelegrafContext<Update.CallbackQueryUpdate<CallbackQuery>>,
@@ -31,12 +27,20 @@ export const handleSuggestedMemePremoderation = async <GAction extends EMemeSugg
       action === EMemeSuggestionCallback.APPROVE_WITHOUT_TEXT || !suggestedMeme.text
         ? undefined
         : suggestedMeme.text
-    await ctx.telegram.sendPhoto(chatIds.memes, suggestedMeme.fileId, {
-      caption,
-    })
+    const publishedMessage = await ctx.telegram.sendPhoto(
+      telegramChat.memes,
+      suggestedMeme.fileId,
+      {
+        caption,
+      },
+    )
     await ctx.editMessageReplyMarkup({
       inline_keyboard: [[Markup.button.callback(`✅ by ${user}`, ECallback.IGNORE)]],
     })
+    await db
+      .update(botMemeSuggestions)
+      .set({ publishedId: publishedMessage.message_id })
+      .where(eq(botMemeSuggestions.id, suggestionId))
   } else if (action === EMemeSuggestionCallback.DECLINE) {
     await ctx.editMessageReplyMarkup({
       inline_keyboard: [[Markup.button.callback(`❌ by ${user}`, ECallback.IGNORE)]],
