e688ab9c49f8b406ac41df9d4130e60c886fcd0d
Vishtar <viktor.kishchuk@gmail.com>
2024-08-13 18:19:03 +0400

    memesearch: disabled proxy-checker & proxy-finder

--- services/constants/serviceSettings.ts
+++ services/constants/serviceSettings.ts
@@ -1,7 +1,6 @@
-import { tgParser, downloader, ocr, checkProxies } from '..'
+import { tgParser, downloader, ocr } from '..'
 import { CYCLE_SLEEP_TIMEOUT } from '../../constants'
 import { amqpQueueMessageCounter } from '../promtail-workers'
-import { findNewProxies } from '../utils'
 
 export const serviceSettings = [
   {
@@ -19,17 +18,17 @@ export const serviceSettings = [
     name: 'ocr',
     service: ocr,
   },
-  {
-    name: 'proxy-checker',
-    service: checkProxies,
-  },
-  {
-    name: 'proxy-finder',
-    service: findNewProxies,
-    loopSettings: {
-      afterCallbackDelayMs: 1000 * 60 * 60,
-    },
-  },
+  // {
+  //   name: 'proxy-checker',
+  //   service: checkProxies,
+  // },
+  // {
+  //   name: 'proxy-finder',
+  //   service: findNewProxies,
+  //   loopSettings: {
+  //     afterCallbackDelayMs: 1000 * 60 * 60,
+  //   },
+  // },
   {
     name: 'promtail-workers',
     service: amqpQueueMessageCounter,
